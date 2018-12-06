1e8c3cc1a75f365bb2f43d98c5beb8995ca8a708
Shane <Shane@DESKTOP-3A8FODV>
2018-12-06 12:55:03 +0000

    db connection

---
+++ app/start.php
@@ -0,0 +1,8 @@
+<?php
+/*SETUP*/
+
+ini_set('display_errors', 'On');
+
+define('APP_ROOT', __DIR__);
+
+echo APP_ROOT;
--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>CMS</title>
-  </head>
-  <body>
-    <h1>CMS</h1>
-  </body>
-</html>
+<?php
+
+require 'app/start.php';
