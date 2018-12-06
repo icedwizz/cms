c72bdfec5cea3739902ee2333ad45894c74c0ab4
Shane <Shane@DESKTOP-3A8FODV>
2018-12-06 14:22:57 +0000

    password set htaccess

--- app/start.php
+++ app/start.php
@@ -4,5 +4,20 @@
 ini_set('display_errors', 'On');
 
 define('APP_ROOT', __DIR__);
+define('BASE_URL', 'http://localhost/cms/');
 
-echo APP_ROOT;
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+try{
+$conn = new PDO("mysql:host=$servername; dbname=cms", $username,$password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo "Connected Successfully";
+}
+
+catch(PDOException $e){
+  echo "Connection failure: " . $e->getMessage();
+}
